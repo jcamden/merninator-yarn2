e94e7127b2ec418295331ce572e7da925142236c
jcamden <john.camden@gmail.com>
2020-09-15 14:55:07 -0700

    tsconfig.json, .vscode/settings.json, README.md with markdownlint

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+enum Turtle {
+  farm = "farm",
+  monkey = "monkey",
+}
+
+const mouse = (turtle: Turtle.farm | Turtle.monkey) => {
+  console.log(turtle);
+};
+
+mouse(Turtle.monkey);
+
 export default function App() {
   return <h1>Hello, world!</h1>;
 }
